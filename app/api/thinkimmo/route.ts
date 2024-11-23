d9a758a8809685dcffdf62739f47f4f3cd03f586
EdwxnRh <78694324+EdwxnRh@users.noreply.github.com>
2024-11-23 16:00:42 +0100

    initialized api functionality

--- app/api/thinkimmo/route.ts
+++ app/api/thinkimmo/route.ts
@@ -1,30 +1,8 @@
 "use server"
-import type {NextApiRequest, NextApiResponse} from 'next'
 
-type ResponseData = {
-	message: String
-	
-}
+//const token = "6a01979b-64e1-46cd-adff-69ff574f27c2";
 
-const token = "6a01979b-64e1-46cd-adff-69ff574f27c2";
-
-//get listings
-export default async function POST() {
-
-	fetch("https://api.thinkimmo.com/organization/integrations/alert", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"AuthorizationAPI": token,
-		},
-		body: JSON.stringify({
-			"customer_id": "0",
-  			"alertSettings": {},
-			"filter": {}
-		}),
-	}).then(res => {
-		const data = res.json();
-
-		console.log(data);
-	});
+export async function POST(request: Request) {
+	const res = await request.json()
+  return Response.json({ res })
 }
