4b0713413a7eaaeee1f3b9eb0ddcbb3462b92b74
Timon Ensel <forgottosave.ensel@tum.de>
2024-11-24 04:06:27 +0100

    add personal information

--- app/what-next/page.tsx
+++ app/what-next/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
                                 Good financial documents will you help with getting a loan at the bank and impress potential sellers.
                                 We recomment you prepare:
                                 <ul className="list-disc list-inside">
-                                  <li>A SCHUFA report (Schufa Auskunft)</li>
-                                  <li>A proof of salary (Gehaltsnachweis)</li>
+                                  <li>A <a href="https://www.schufa.de/" className="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">Schufa report</a> (germ.: Schufa Auskunft)</li>
+                                  <li>A proof of salary (germ.: Gehaltsnachweis)</li>
                                   <li>Calculation of your total household income:
                                     <br/>Salaries, 13th Salaries, Bonuses, Other incomes</li>
                                   <li>Calculation of your total equities:
@@ -110,12 +110,12 @@ export default function Home() {
                 </section>
             </li>
         </ol>
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-white dark:bg-gray-900 -mt-10 border-t-4 border-primary">
             <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-primary">Numbers In Germany</h2>
                 <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
                     <div className="flex flex-col items-center justify-center">
-                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">4-12 months</dt>
+                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">6-12 months</dt>
                         <dd className="font-light text-gray-500 dark:text-gray-400">in average, to buy a home</dd>
                     </div>
                     <div className="flex flex-col items-center justify-center">
